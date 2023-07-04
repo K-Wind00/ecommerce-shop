73b268295bc4d25d38f128b11eb978f3daf55142
Kacper <k.wiater00@gmail.com>
2023-07-04 17:00:09 +0200

    added login component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,21 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DefaultModule } from './layouts/default/default.module';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
+import { FullpageModule } from './layouts/fullpage/fullpage.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     DefaultModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FullpageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
